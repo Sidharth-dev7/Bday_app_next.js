416786b8d371cb79ac1930d2574406af63873e2c
Sidharth <sunilsidharth411@gmail.com>
2025-04-27 23:54:43 +0530

    final

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.happybday',
+  appName: 'Hapy Bday App',
+  webDir: 'out'
+};
+
+export default config;
